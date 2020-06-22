2ad38d09c9e3cb49292cc4eb4ee3e7b9f601be7a
Thiago André <66754516+thiago1590@users.noreply.github.com>
2020-06-22 15:32:55 -0300

    commit

--- JavaScript/Fundamentos/caderno_fundamentos.js
+++ JavaScript/Fundamentos/caderno_fundamentos.js
@@ -21,7 +21,7 @@ prod1['Desconto Legal'] = 0.40 //evitar atributos com espaço
 
 const prod2 = {nome:'Camisa Polo', preco:79.90}
 
-//Refência
+//Referência
 const a = {name:'Teste'}
 const b = a //objetos possuem copia por referencia
 
@@ -29,8 +29,52 @@ const c = 3
 let d = c
 d++ //d == 4, c ==3   //tipos primitivos possuem cópia por valor
 
-//defined 
+//defin
 let valor
 console.log(valor) //undefined
 console.log(valor2) //is not defined
 valor = null //foi inicializada mas nao aponta p/ nenhum endereço
+
+//armazenando uma funcao em uma variavel
+const imprimirSoma = function (a,b){
+    console.log(a+b)
+}
+
+//armazemando uuma funcao arrow em uma variavel
+const soma = (a,b) => {
+    return a + b
+}
+
+// retorno implícito
+const subtracao = (a,b) => a - b
+
+//operador destructuring
+const pessoa = {
+    nome:'Thiago',
+    idade:5,
+    endereco:{
+        logradouro:'Rua Carlos Palut',
+        numero:426
+    }
+}
+
+const {nome, idade} = pessoa
+
+const{nome:n,idade:i} = pessoa
+
+const{sobrenome, bemHumorada = true} = pessoa
+
+const {endereco: {logradouro, numero, cep}} = pessoa
+
+const [a] = [10] //para array
+const [n1, , n3, , n5, n6 = 0] = [10, 7, 9, 8]
+
+//operadores logicos
+function compras(trabalho1, trabalho2) {
+    const comprarSorvete = trabalho1 || trabalho2
+    const comprarTv50 = trabalho1 && trabalho2
+    const comprarTv32 = trabalho1 != trabalho2
+    const manterSaudavel = !comprarSorvete // operador unário
+
+    return { comprarSorvete, comprarTv50, comprarTv32, manterSaudavel }//declaraçao simplificada
+}
--- JavaScript/Fundamentos/teste.js
+++ JavaScript/Fundamentos/teste.js
@@ -1,19 +1,15 @@
-const a = {name:'Teste'}
-const b = a //objetos possuem copia por referencia
+function compras(trabalho1, trabalho2) {
+    const comprarSorvete = trabalho1 || trabalho2
+    const comprarTv50 = trabalho1 && trabalho2
+    const comprarTv32 = trabalho1 != trabalho2
+    const manterSaudavel = !comprarSorvete // operador unário
 
-const c = 3
-let d = c
-d++ //d == 4, c ==3   //tipos primitivos possuem cópia por valor
-
-let valor
-console.log(valor) //undefined
-
-console.log(valor2) //is not defined
-
-valor = null //foi inicializada mas nao aponta p/ nenhum endereço
-
-const produto = {}
-console.log(produto.preco)
+    return { comprarSorvete, comprarTv50, comprarTv32, manterSaudavel }//declaraçao simplificada
+}
 
 
 
+
+
+
+   
