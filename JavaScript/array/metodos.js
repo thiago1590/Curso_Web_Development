ccede43c8b965ba6341fa52c7e4a337184513a9a
Thiago André <66754516+thiago1590@users.noreply.github.com>
2020-06-29 17:20:38 -0300

    commit

---
+++ JavaScript/array/metodos.js
@@ -0,0 +1,28 @@
+aprovados = [thiago,bia,julia,carol,junior]
+
+//splice
+aprovados.splice(1,0,'Elemento1','Elemento2')
+
+//sort
+aprovados.sort
+
+//pop
+aprovados.pop()//remove ultimo elemento
+
+//push
+aprovados.push('Gustavo') //adiciona no final
+
+//shift
+aprovados.shift() //remove o primeiro
+
+//unshift
+aprovados.unshift('lais') //adiciona no inicio
+
+//splice
+aprovados.splice(2, 0,'Eliana','rodrigo') //adiciona 2e na pos 0
+aprovados.splice(3,1) //remove
+
+//slice
+const aprovados2 = pilotos.slice(2,4) //novo array a partir da pos 2 até 3
+
+
