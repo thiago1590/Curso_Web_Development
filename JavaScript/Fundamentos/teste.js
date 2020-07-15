c1852f945d528e570c4423b818c80a382c49a8c5
Thiago André <66754516+thiago1590@users.noreply.github.com>
2020-07-15 14:24:31 -0300

    commit

--- JavaScript/Fundamentos/teste.js
+++ JavaScript/Fundamentos/teste.js
@@ -1,11 +1,40 @@
-let dobro = function(a) {
-    return 2 * a
+function compareTriplets(ar) {
+  let reverse = []
+  let soma_diag_p = 0
+  let soma_diag_s = 0
+  let resultado
+  for(let i = ar.length;i>0;i--){
+      let z = i-1
+    reverse.push(z)
+  }
+  
+  for(let i in ar){
+      soma_diag_p += ar[i][i]
+      soma_diag_s += ar[reverse[i]][i]
+  }
+  
+  resultado = soma_diag_p - soma_diag_s
+ 
+  if(resultado < 0){
+      resultado = resultado * (-1)
+  }
+  console.log(soma_diag_p)
+  console.log(soma_diag_s)
+  console.log(resultado)
+  return(resultado)
 }
 
-dobro = (a) => {
-    return a * 2
-}
+var mochila = new Array()
+var item1=[11,2,4],
+    item2=[4,5,6];
+    item3=[10,8,-12];
+    
 
+    mochila.push(item1);
+    mochila.push(item2);
+    mochila.push(item3);
+
+compareTriplets(mochila)
 
 
 
